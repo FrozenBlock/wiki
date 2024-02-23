578dfea716d0cc68c8d080a486ac9b2f52e57115
Treetrain1 <treetrain1@gmail.com>
2024-02-22 20:02:59 -0600

    add discord button + nearly center social icons (still not centered)

--- site/src/jsMain/kotlin/net/frozenblock/net/components/sections/Footer.kt
+++ site/src/jsMain/kotlin/net/frozenblock/net/components/sections/Footer.kt
@@ -6,13 +6,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Color
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.forms.ButtonSize
+import com.varabyte.kobweb.silk.components.forms.ButtonVars
 import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.layout.SimpleGrid
 import com.varabyte.kobweb.silk.components.layout.numColumns
@@ -27,6 +31,7 @@ import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import com.varabyte.kobweb.silk.theme.colors.ColorSchemes
 import net.frozenblock.net.CircleButtonVariant
 import net.frozenblock.net.toSitePalette
+import org.jetbrains.compose.web.css.CSSColorValue
 import org.jetbrains.compose.web.css.cssRem
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.dom.Span
@@ -37,12 +42,26 @@ val FooterStyle by ComponentStyle.base {
         .padding(topBottom = 1.5.cssRem, leftRight = 10.percent)
 }
 
-private class SocialButton(val link: String, val icon: String)
+private class SocialButton(
+    val link: String,
+    val icon: String,
+    val buttonColor: CSSColorValue? = null,
+    val hoverButtonColor: CSSColorValue? = null,
+    val pressButtonColor: CSSColorValue? = null
+)
 
 @Composable
 private fun SocialButtonItem(social: SocialButton) {
     Link(social.link) {
-        Button(onClick = {}, variant = CircleButtonVariant, colorScheme = ColorSchemes.Monochrome) {
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .setVariable(ButtonVars.BackgroundDefaultColor, social.buttonColor)
+                .setVariable(ButtonVars.BackgroundHoverColor, social.hoverButtonColor)
+                .setVariable(ButtonVars.BackgroundPressedColor, social.pressButtonColor),
+            variant = CircleButtonVariant,
+            size = ButtonSize.LG
+        ) {
             Image(social.icon, height = 32)
         }
     }
@@ -52,11 +71,47 @@ private fun SocialButtonItem(social: SocialButton) {
 private fun SocialBar() {
     val socials = remember {
         listOf(
-            SocialButton("https://github.com/FrozenBlock", "/social/github.svg")
+            SocialButton(
+                "https://discord.com/invite/frozenblock-modding-oasis-780746010614956112",
+                "/social/discord.svg",
+                Color.Companion.rgb(0x5865F2),
+                Color.Companion.rgb(0x7883f4)
+            ),
+            SocialButton(
+                "https://github.com/FrozenBlock",
+                "/social/github.svg",
+                Color.Companion.rgb(0xFFFFFF),
+                Color.Companion.rgb(0xC0C0C0)
+            ),
+
+            // these buttons are just for testing
+            SocialButton(
+                "https://github.com/FrozenBlock",
+                "/social/github.svg",
+                Color.Companion.rgb(0xFFFFFF),
+                Color.Companion.rgb(0xC0C0C0)
+            ),
+            SocialButton(
+                "https://github.com/FrozenBlock",
+                "/social/github.svg",
+                Color.Companion.rgb(0xFFFFFF),
+                Color.Companion.rgb(0xC0C0C0)
+            ),
+            SocialButton(
+                "https://github.com/FrozenBlock",
+                "/social/github.svg",
+                Color.Companion.rgb(0xFFFFFF),
+                Color.Companion.rgb(0xC0C0C0)
+            )
         )
     }
 
-    SimpleGrid(numColumns(1, md = 5)) {
+    SimpleGrid(
+        numColumns(1, md = 3, lg = 5),
+        Modifier
+            .padding(2.cssRem)
+            .gap(1.5.cssRem)
+    ) {
         socials.forEach { social -> SocialButtonItem(social) }
     }
 }
@@ -64,7 +119,7 @@ private fun SocialBar() {
 @Composable
 fun Footer(modifier: Modifier = Modifier) {
     Box(FooterStyle.toModifier().then(modifier), contentAlignment = Alignment.Center) {
-        Column {
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
             SocialBar()
 
             Span(Modifier.textAlign(TextAlign.Center).toAttrs()) {
